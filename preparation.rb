4a3c29e87a78310666d0269441786c2c648589b2
pychap <pychapster@gmail.com>
2016-06-24 17:23:38 -0700

    Uploading 1st file

---
+++ preparation.rb
@@ -0,0 +1,5 @@
+# preparation.rb
+movies = {:jaws => 1975, :star_wars => 1978, :taxi => 1972, :james_bond => 1963}
+# puts movies [[:jaws], [:star_wars], [:taxi], [:james_bond]]
+
+puts 5+14+19+50+32
