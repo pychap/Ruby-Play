bbd84e926dd36321ee0a5a2790ee418e27ee4c34
pychap <pychapster@gmail.com>
2016-07-28 18:21:09 -0700

    Posting true/false method tester

--- pine_lrn_to_program/methods.rb
+++ pine_lrn_to_program/methods.rb
@@ -1,49 +1,87 @@
-puts 'Hello, and thank you for taking the time to'
-puts 'help me with this experiment.  My experiment'
-puts 'has to do with the way people feel about'
-puts 'Mexican food.  Just think about Mexican food'
-puts 'and try to answer every question honestly,'
-puts 'with either a "yes" or a "no".  My experiment'
-puts 'has nothing to do with bed-wetting.'
+# puts 'Hello, and thank you for taking the time to'
+# puts 'help me with this experiment.  My experiment'
+# puts 'has to do with the way people feel about'
+# puts 'Mexican food.  Just think about Mexican food'
+# puts 'and try to answer every question honestly,'
+# puts 'with either a "yes" or a "no".  My experiment'
+# puts 'has nothing to do with bed-wetting.'
+# puts
+
+# goodAnswer = false
+# while (not goodAnswer)
+#   puts 'Do you like eating tacos?'
+#   answer = gets.chomp.downcase
+#   if (answer == 'yes' or answer == 'no')
+#     goodAnswer = true
+#   else
+#     puts 'Please answer "yes" or "no".'
+#   end
+# end
+
+# # this answer is collected
+# goodAnswer = false
+# while (not goodAnswer)
+#   puts 'Do you wet the bed?'
+#   answer = gets.chomp.downcase
+#   if (answer == 'yes' or answer == 'no')
+#     goodAnswer = true
+#     if answer == 'yes'
+#       wetsBed = true
+#       else
+#         wetsBed = false
+#       end
+#   else
+#     puts 'Please answer "yes" or "no".'
+#   end
+# end
+
+# puts
+# puts 'DEBRIEFING:'
+# puts 'Thank you for taking the time to help with'
+# puts 'this experiment.  In fact, this experiment'
+# puts 'has nothing to do with Mexican food.  It is'
+# puts 'an experiment about bed-wetting.  The Mexican'
+# puts 'food was just there to catch you off guard'
+# puts 'in the hopes that you would answer more'
+# puts 'honestly.  Thanks again.'
+# puts
+# puts wetsBed
+
+def ask question
+  goodAnswer = false
+  while (not goodAnswer)
+    puts question
+    reply = gets.chomp.downcase
+
+    if (reply == 'yes' or reply == 'no')
+      goodAnswer = true
+      if reply == 'yes'
+        answer = true
+      else
+        answer = false
+      end
+    else
+      puts 'Please answer yes or no.'
+    end
+  end
+  answer
+end
+
+puts 'Hello, and thank you for...'
 puts
 
-goodAnswer = false
-while (not goodAnswer)
-  puts 'Do you like eating tacos?'
-  answer = gets.chomp.downcase
-  if (answer == 'yes' or answer == 'no')
-    goodAnswer = true
-  else
-    puts 'Please answer "yes" or "no".'
-  end
-end
-
-# this answer is collected
-goodAnswer = false
-while (not goodAnswer)
-  puts 'Do you wet the bed?'
-  answer = gets.chomp.downcase
-  if (answer == 'yes' or answer == 'no')
-    goodAnswer = true
-    if answer == 'yes'
-      wetsBed = true
-      else
-        wetsBed = false
-      end
-  else
-    puts 'Please answer "yes" or "no".'
-  end
-end
-
+ask 'Do you like eating tacos?'      # We ignore this return value.
+ask 'Do you like eating burritos?'
+wetsBed = ask 'Do you wet the bed?'  # We save this return value.
+ask 'Do you like eating chimichangas?'
+ask 'Do you like eating sopapillas?'
+ask 'Do you like eating tamales?'
+puts 'Just a few more questions...'
+ask 'Do you like drinking horchata?'
+ask 'Do you like eating flautas?'
 
 puts
 puts 'DEBRIEFING:'
-puts 'Thank you for taking the time to help with'
-puts 'this experiment.  In fact, this experiment'
-puts 'has nothing to do with Mexican food.  It is'
-puts 'an experiment about bed-wetting.  The Mexican'
-puts 'food was just there to catch you off guard'
-puts 'in the hopes that you would answer more'
-puts 'honestly.  Thanks again.'
+puts 'Thank you for...'
 puts
 puts wetsBed
