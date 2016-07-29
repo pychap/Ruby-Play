57157df5b9045afb8207253dcb1c32abf751a8d8
pychap <pychapster@gmail.com>
2016-07-28 18:03:17 -0700

    Testing to see if this Sublime Git plugin is working properly

---
+++ pine_lrn_to_program/methods.rb
@@ -0,0 +1,49 @@
+puts 'Hello, and thank you for taking the time to'
+puts 'help me with this experiment.  My experiment'
+puts 'has to do with the way people feel about'
+puts 'Mexican food.  Just think about Mexican food'
+puts 'and try to answer every question honestly,'
+puts 'with either a "yes" or a "no".  My experiment'
+puts 'has nothing to do with bed-wetting.'
+puts
+
+goodAnswer = false
+while (not goodAnswer)
+  puts 'Do you like eating tacos?'
+  answer = gets.chomp.downcase
+  if (answer == 'yes' or answer == 'no')
+    goodAnswer = true
+  else
+    puts 'Please answer "yes" or "no".'
+  end
+end
+
+# this answer is collected
+goodAnswer = false
+while (not goodAnswer)
+  puts 'Do you wet the bed?'
+  answer = gets.chomp.downcase
+  if (answer == 'yes' or answer == 'no')
+    goodAnswer = true
+    if answer == 'yes'
+      wetsBed = true
+      else
+        wetsBed = false
+      end
+  else
+    puts 'Please answer "yes" or "no".'
+  end
+end
+
+
+puts
+puts 'DEBRIEFING:'
+puts 'Thank you for taking the time to help with'
+puts 'this experiment.  In fact, this experiment'
+puts 'has nothing to do with Mexican food.  It is'
+puts 'an experiment about bed-wetting.  The Mexican'
+puts 'food was just there to catch you off guard'
+puts 'in the hopes that you would answer more'
+puts 'honestly.  Thanks again.'
+puts
+puts wetsBed
